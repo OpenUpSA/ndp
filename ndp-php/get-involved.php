d62ea6c8631b4723bb06357ed777cbf17488bbab
Greg Kempe <greg@kempe.net>
2015-10-13 22:25:59 +0200

    Remove ndp-html and ndp-php.

--- ndp-php/get-involved.php
+++
@@ -1,2 +0,0 @@
-<h2>GET INVOLVED</h2>
-<p>Would you like  to make changes to this page? Or add the name of a group making an important contribution? Please click <a href="">here</a> to send us a message</p>
--- ndp-php/site-links.php
+++
@@ -1,3 +0,0 @@
-<a href="about.php">ABOUT THIS SITE</a>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;SHARE&nbsp;&nbsp;
-<a href=""><img src="inc/img/twitter.png"/></a>
-<a href=""><img src="inc/img/facebook.png"/></a>
