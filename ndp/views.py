3633874fdb741acf97b0f9022f96880de8d638c2
Greg Kempe <greg@kempe.net>
2015-10-13 21:38:06 +0200

    Sector page and data

--- ndp/views.py
+++ ndp/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from ndp.models import Sector
 
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {
+        'pagename': 'home',
+    })
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', {
+        'pagename': 'about',
+    })
+
+
+def sector(request, slug):
+    sector = get_object_or_404(Sector.objects.filter(slug=slug))
+    return render(request, 'sector.html', {
+        'pagename': 'sector',
+        'sector': sector,
+    })
