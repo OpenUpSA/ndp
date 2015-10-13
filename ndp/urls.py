8ee521f473fa874f1661156bbc32efe2067251bd
Greg Kempe <greg@kempe.net>
2015-10-13 20:38:03 +0200

    Correct Procfile

--- ndp/urls.py
+++ ndp/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'ndp.views.home', name='home'),
+    url(r'^about$', 'ndp.views.about', name='about'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
