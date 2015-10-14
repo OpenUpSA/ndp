29d23bc5538a39d1ec09879ce3e1e8cece511ac0
Greg Kempe <greg@kempe.net>
2015-10-14 08:29:02 +0200

    Dynamic sector links.

--- ndp/views.py
+++ ndp/views.py
@@ -16,14 +16,23 @@ def home(request):
 
 
 def about(request):
+    sectors = list(Sector.objects.all())
+    shuffle(sectors)
+
     return render(request, 'about.html', {
         'pagename': 'about',
+        'sectors': sectors,
     })
 
 
 def sector(request, slug):
     sector = get_object_or_404(Sector.objects.filter(slug=slug))
+
+    sectors = list(Sector.objects.all())
+    shuffle(sectors)
+
     return render(request, 'sector.html', {
         'pagename': 'sector',
         'sector': sector,
+        'sectors': sectors,
     })
